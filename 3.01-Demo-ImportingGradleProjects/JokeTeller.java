a9a4d5c6a225d617d8c6d64186a8ec1d5024fcee
vladimirjeune <randyjeune@gmail.com>
2018-09-24 17:22:32 -0400

    feat: Not sure how was supposed to work
    Could not get Gradle Wrapper to work as expected and had trouble
    creating the Java file.  Saving so can continue.

---
+++ 3.01-Demo-ImportingGradleProjects/JokeTeller.java
@@ -0,0 +1,11 @@
+package com.udacity.gradle;
+
+public class JokeTeller {
+    public JokeTeller() {
+        System.out.println("Tell a joke");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Telling a joke");
+    }
+}
