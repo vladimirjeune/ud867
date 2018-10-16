52e4908679f64de8e2ce3d3a4d917b7a68420742
vladimirjeune <randyjeune@gmail.com>
2018-10-16 17:33:43 -0400

    feat: Start to set up Unit Testing
    Create the folders and Classes necessary to to set up testing.
    Currently, you got all the red squigglies to disappear and
    everything seems to know that it exists, but you have introduced
    duplicates somewhere in the build.gradle. There is an extra thing
    in your code due to transitive dependency. You have to remove the
    extra and different dependency so that the conflict can be resolved
    you can run the android tests. You will have to do this in a future
    commit.

--- 4.10-Exercise-SetUpUnitTesting/app/src/androidTest/java/com/example/android/clickcounter/androidtesting/ClickFragmentTest.java
+++ 4.10-Exercise-SetUpUnitTesting/app/src/androidTest/java/com/example/android/clickcounter/androidtesting/ClickFragmentTest.java
@@ -4,17 +4,42 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.example.android.clickcounter.ClickActivity;
+import com.example.android.clickcounter.R;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class ClickFragmentTest {
 
     @Rule
-    public ActivityTestRule<ClickActivity> mActivityRule = new ActivityTestRule<>(ClickActivity.class);
+    public ActivityTestRule<ClickActivity> mActivityRule
+            = new ActivityTestRule<>(ClickActivity.class);
 
     // TODO: Put a couple reasonable Espresso tests here.
+    @Test
+    public void verifyClickButtonIncrementsValue() {
+        String zeroValue = "0";
+        String onePlusZeroValue = "1";
+
+        // Find the initial value that should be zero
+        onView(withId(R.id.click_count_text_view)).check(matches(withText(zeroValue)));
+
+        // Find the button and click it
+        onView(withId(R.id.click_button)).perform(click());
+
+        // Find the new value and make sure it has incremented
+        onView(withId(R.id.click_count_text_view)).check(matches(withText(onePlusZeroValue)));
+
+    }
+
 
 
 }
