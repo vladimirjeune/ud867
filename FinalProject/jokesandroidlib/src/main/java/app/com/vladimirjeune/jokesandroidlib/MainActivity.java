7de309dc0692c01975ff470ab335b050b59e6178
vladimirjeune <randyjeune@gmail.com>
2018-10-23 07:51:27 -0400

    fix: Fix app not running because of Theme conflict between app and AndroidLibary
    The Android Library was given a theme because it was not able to be tested
    without it. You may have to figure out another way to do this later.

--- FinalProject/jokesandroidlib/src/main/java/app/com/vladimirjeune/jokesandroidlib/MainActivity.java
+++ FinalProject/jokesandroidlib/src/main/java/app/com/vladimirjeune/jokesandroidlib/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_joke);
 
         Intent intent = getIntent();
         String joke = intent.getStringExtra(JOKE_TAG);
