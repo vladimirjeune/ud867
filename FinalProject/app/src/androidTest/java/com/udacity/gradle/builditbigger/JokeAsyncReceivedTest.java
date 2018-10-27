13395c8dd277a78ae7c7f3f20462907cbf25cc83
vladimirjeune <randyjeune@gmail.com>
2018-10-27 10:06:39 -0400

    fix: Add code to test that text is displayed on joke showing Activity
    Add code to test that your Async task successfully retrieves a non-empty
    string. For a refresher on setting up Android tests, check out demo 4.09.
    
    Create an androidTest directory in correct location. Make sure you are
    in the right one, or will not have access to the appropriate functions.
    Make sure to have all the correct Espresso stuff in the build.gradle for
    the part you are testing.

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAsyncReceivedTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAsyncReceivedTest.java
@@ -10,11 +10,15 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class JokeAsyncReceivedTest {
 
+    // We have access to MainActivity
     @Rule
     public ActivityTestRule<MainActivity> mMainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
@@ -32,6 +36,10 @@ public class JokeAsyncReceivedTest {
     @Test
     public void jokeAsyncReceivedTest() {
         onView(withId(R.id.tell_joke_button)).perform(click());
+
+        // Now should be on AndroidLib's MainActivity
+        onView(withId(R.id.tv_joke)).check(matches(not(withText(""))));
+
     }
 
 }
