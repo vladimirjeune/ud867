24db0e67f1ce54ee795f3f71c39707c49ff4ee77
vladimirjeune <randyjeune@gmail.com>
2018-11-03 10:39:56 -0400

    feat: Add ProgressBar to 'Paid'
    Add ProgressBar to, 'Paid', so that you can see it when you
    go to the joke, before it comes up. It will take time because
    it has to go the the, backend, which is simulating the cloud.

--- FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
+
+        // Show ProgressBar
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MainActivityFragment mainActivityFragment = (MainActivityFragment) fragmentManager.findFragmentById(R.id.fragment);
+        mainActivityFragment.showProgressBar();
 
         new EndpointsAsyncTask().execute(this);  // Get joke and display it
 
--- FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 
 /**
@@ -12,15 +14,56 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    public static final String TAG = MainActivityFragment.class.getSimpleName();
+    private RelativeLayout mRelativeLayout;
+    private ProgressBar mProgressBar;
+
     public MainActivityFragment() {
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
+        mProgressBar = root.findViewById(R.id.pb_pause_for_effect);
+        mRelativeLayout = root.findViewById(R.id.rl_main_activity);
+
+        hideProgressBar();
+
         return root;
+    }
+
+
+    /**
+     * HIDEPROGRESSBAR - Will hide the progressBar that is shown on the main page. Initially, should be
+     * off, but will need to be turned off after user goes to next activity.
+     */
+    void hideProgressBar() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mRelativeLayout.setVisibility(View.VISIBLE);
+    }
+
+
+    /**
+     * ONSTART
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        hideProgressBar();
+    }
+
+
+    /**
+     * SHOWPROGRESSBAR - Will show the indeterminate progressbar that is usually hidden, to indicate that
+     * we are waiting for the other Activity to start. May take a while because of the need to go to the
+     * cloud(if this were an app that used the internet cloud, instead of just local.)
+     */
+    void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
 }
